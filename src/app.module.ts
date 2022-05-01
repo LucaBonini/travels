a468788790bc082028fa435e00d5647a7087237a
Luca Bonini <luca.bonini93@gmail.com>
2022-05-01 09:15:24 +0200

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { TravelModule } from './modules/travel/travel.module';
@@ -60,7 +59,6 @@ import { config } from 'process';
       })
     })
   ],
-  controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {}
